3387ba0cca46f1eaab849cf769673a6e2f5aefd1
Salomao S <souza95salomao@gmail.com>
2023-02-15 03:04:39 +0000

    feat: treasury

---
+++ src/com/checkout/Treasury.java
@@ -0,0 +1,67 @@
+package com.checkout;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Treasury {
+	private final String FILE_NAME;
+	private Double cash = 0.00;
+
+	public Treasury(String fileName) {
+		FILE_NAME = String
+			.format("%1$s/treasury.csv", fileName);
+	}
+
+	public Double getCash() {
+		return this.cash;
+	}
+
+	public void addCash(double ammount) {
+		this.cash += ammount;
+		sendCashToTreasury();
+
+		System.out.println("Updated cash: " + cash);
+	}
+
+	public void withdrawCash(double ammount) {
+		this.cash -= ammount;
+		sendCashToTreasury();
+
+		System.out.println("Updated cash: " + cash);
+	}
+
+	public void sendCashToTreasury() {
+		try {
+			FileWriter fileWriter = new FileWriter(FILE_NAME);
+			PrintWriter printWriter = new PrintWriter(fileWriter);
+
+			String cashFormatted = String.format("%.2f", this.cash);
+
+			printWriter.printf("ammount");
+			printWriter.printf(cashFormatted);
+
+			printWriter.close();
+			fileWriter.close();
+		} catch(IOException exception) {
+			System.out.println(exception);
+		}
+	}
+
+	public void getCashInfoFromTreasury() {
+		try {
+  		Boolean noCash = cash.isNaN() || cash.equals(null);
+			Path filePath = Path.of(FILE_NAME);
+
+			String cash = Files
+				.readAllLines(filePath)
+				.get(1);
+
+			this.cash = noCash ? Double.parseDouble(cash) : 0.00;
+		} catch(IOException exception) {
+			System.out.println(exception);
+		}
+	}
+}
