8e70e324f77c7e105e1e2aaeed86739a042a99c1
Salomao S <souza95salomao@gmail.com>
2023-06-25 01:22:49 +0000

    chore: refactoring test classes using Mockito

--- src/test/java/com/softea/factory/OrderFactory.java
+++ src/test/java/com/softea/factory/OrderFactory.java
@@ -5,22 +5,26 @@
 
 package com.softea.factory;
 
-import com.softea.modules.dto.OrderDTO;
-import com.softea.modules.entity.Product;
+import com.softea.modules.entity.*;
 import java.util.List;
 
 public class OrderFactory {
-	public static OrderDTO create(
+	public static Order create(
+			String id,
 			long customer,
+			int table,
 			List<Product> items) {
-		return new OrderDTO()
+		return new Order()
 			.setCustomer(customer)
-			.setOrderItems(items);
+			.setOrderItems(items)
+			.setTable(table);
 	}
 
-	public static OrderDTO create() {
+	public static Order create() {
 		return create(
+			"1822",
 			183788181881384L,
+			9,
 			List.of(ProductFactory.create()));
 	}
 }
--- src/test/java/com/softea/service/MenuServiceTests.java
+++ src/test/java/com/softea/service/MenuServiceTests.java
@@ -5,56 +5,82 @@
 
 package com.softea.service;
 
+import java.util.Optional;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
+import com.softea.factory.ProductFactory;
 import com.softea.modules.dto.ProductDTO;
+import com.softea.modules.entity.Product;
 import com.softea.modules.handler.ProductNotFoundException;
+import com.softea.modules.repository.IProductRepository;
+import com.softea.modules.repository.ProductRepository;
 import com.softea.modules.service.MenuService;
-import com.softea.repository.ProductRepository;
+import jakarta.validation.ConstraintViolationException;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MenuServiceTests {
 	private MenuService menu;
+	private IProductRepository pr;
 	
 	@BeforeAll
 	void setup() {
-		ProductRepository pr = new ProductRepository();
+		pr = Mockito.mock(ProductRepository.class);
 		menu = new MenuService(pr);
+	}
+
+	@AfterAll
+	void drop() {
+		Mockito.reset(pr);
 	}
 
 	@Test
 	void should_not_find_a_product() {
+		String id = "12345";
+		
+		Mockito.when(pr.findById(id)).thenReturn(
+			Optional.empty());
+		
 		Assertions.assertThrows(
 			ProductNotFoundException.class,
-			()->menu.getProduct("12345"));
+			()->menu.getProduct(id));
 	}
 
 	@Test
 	void should_find_a_product() {
-		ProductDTO dto = new ProductDTO()
-			.setSku("Bubble Tea")
-			.setPrice(2.45d);
-		String id = String.valueOf(dto.hashCode());
-		menu.saveProduct(dto);
+		String id = "1irjq8d8j1nr8dajam";
+		
+		Mockito.when(pr.findById(id)).thenReturn(
+			Optional.of(ProductFactory.create()));
+		
 		Assertions.assertDoesNotThrow(
 			()->menu.getProduct(id));
 	}
 
 	@Test
-	void should_throw_an_exception_saving_a_product() {
-		ProductDTO dto = new ProductDTO(null, null);
+	void should_fail_while_saving_a_product() {
+		ProductDTO dto = new ProductDTO(null, 0d);
+		
+		Mockito.when(pr.save(dto)).thenThrow(
+			ConstraintViolationException.class);
+		
 		Assertions.assertThrows(
-			Exception.class,
+			ConstraintViolationException.class,
 			()->menu.saveProduct(dto));
 	}
 
 	@Test
 	void should_save_a_product_without_fail() {
-		ProductDTO dto = new ProductDTO()
-			.setSku("Black Tea")
-			.setPrice(7.95d);
+		ProductDTO dto = new ProductDTO(
+			"Black Tea",7.95d);
+	
+		Mockito.when(pr.save(dto)).thenReturn(
+			ProductFactory.create());
+
 		Assertions.assertDoesNotThrow(
 			()->menu.saveProduct(dto));
 	}
