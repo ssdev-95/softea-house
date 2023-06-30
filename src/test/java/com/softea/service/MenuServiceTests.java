24cfd20a19eb26b77c2a82bee8aab06d9b2a2c50
Salomao S <souza95salomao@gmail.com>
2023-06-30 01:32:14 +0000

    test: update assertions

--- src/test/java/com/softea/service/MenuServiceTests.java
+++ src/test/java/com/softea/service/MenuServiceTests.java
@@ -38,30 +38,36 @@ public class MenuServiceTests {
 
 	@Test
 	void should_not_find_a_product() {
-		String id = "12345";
+		final String id = "12345";
 		
 		Mockito.when(pr.findById(id)).thenReturn(
 			Optional.empty());
 		
-		Assertions.assertThrows(
+		var exception = Assertions.assertThrows(
 			ProductNotFoundException.class,
 			()->menu.getProduct(id));
+
+		Assertions.assertEquals(
+			"[EXCEPTION] Product not found",
+			exception.getMessage());
 	}
 
 	@Test
 	void should_find_a_product() {
-		String id = "1irjq8d8j1nr8dajam";
+		final String id = "1irjq8d8j1nr8dajam";
 		
 		Mockito.when(pr.findById(id)).thenReturn(
 			Optional.of(ProductFactory.create()));
 		
-		Assertions.assertDoesNotThrow(
+		var product = Assertions.assertDoesNotThrow(
 			()->menu.getProduct(id));
+
+		Assertions.assertNotNull(product);
 	}
 
 	@Test
-	void should_fail_while_saving_a_product() {
-		ProductDTO dto = new ProductDTO(null, 0d);
+	void should_fail_saving_a_product() {
+		final ProductDTO dto = new ProductDTO(null, 0d);
 		
 		Mockito.when(pr.save(dto)).thenThrow(
 			ConstraintViolationException.class);
@@ -73,13 +79,15 @@ public class MenuServiceTests {
 
 	@Test
 	void should_save_a_product_without_fail() {
-		ProductDTO dto = new ProductDTO(
+		final ProductDTO dto = new ProductDTO(
 			"Black Tea",7.95d);
 	
 		Mockito.when(pr.save(dto)).thenReturn(
 			ProductFactory.create());
 
-		Assertions.assertDoesNotThrow(
+		var product = Assertions.assertDoesNotThrow(
 			()->menu.saveProduct(dto));
+
+		Assertions.assertNotNull(product);
 	}
 }
